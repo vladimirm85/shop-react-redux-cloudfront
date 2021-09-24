1b14b6efcc937095b90ee945d0aa047c712dadfc
vladimirm85 <vladimirm85@gmail.com>
2021-09-24 17:14:57 +0300

    refactor: change product api path, minor changes in CSVFileImport

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://sal5bp3s48.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://3w66o9az06.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
