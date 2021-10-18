93bec457814dc80e95b5861cead183ef0cb9031a
vladimirm85 <vladimirm85@gmail.com>
2021-10-18 10:04:29 +0300

    refactor: authorization

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -41,10 +41,13 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file.name),
       },
+      headers: {
+        Authorization: `Basic ${localStorage.getItem('authorization_token')}`,
+      },
     });
     console.log('File to upload: ', file.name);
-    console.log('Uploading to: ', response.data);
-    const result = await fetch(response.data, {
+    console.log('Uploading to: ', response.data.signedUrl);
+    const result = await fetch(response.data.signedUrl, {
       method: 'PUT',
       body: file,
       headers: {
